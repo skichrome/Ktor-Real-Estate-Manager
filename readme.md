2f1ca4a80f759b711335b10c2b3f40fb6bc0446c
skichrome <toni.campeol@outlook.fr>
2019-07-26 16:32:55 +0200

    [Initial Commit] Adding base project files to version control

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Real Estate Manager Ktor Backend
+
+This application is developed to be used with the Android app 
+[Real Estate Manager](https://github.com/skichrome/RealEstateManager)
---
+++ src/main/kotlin/com/skichrome/Program.kt
@@ -0,0 +1,41 @@
+package com.skichrome
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.html.respondHtml
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import kotlinx.html.*
+
+fun Application.main()
+{
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing)
+    {
+        get("/")
+        {
+            call.respondText("Hello world", ContentType.Text.Html)
+        }
+        get("/{field}")
+        {
+            val args = call.parameters["field"] ?: ""
+            call.respondText("unsupported value entered : $args")
+        }
+        get("/html")
+        {
+            call.respondHtml {
+                head { title { +"Html response test" } }
+                body {
+                    h1("title") { +"Hello World" }
+                    p { +"An hello world test with Ktor." }
+                }
+            }
+        }
+    }
+}
