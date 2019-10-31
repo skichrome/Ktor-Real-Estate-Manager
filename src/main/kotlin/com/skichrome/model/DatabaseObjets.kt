5eec348721bc86ba6cdd5ec04fb80fc213afeac4
skichrome <toni.campeol@outlook.fr>
2019-10-31 16:40:17 +0100

    [Database] Update tables column names.

--- src/main/kotlin/com/skichrome/model/DatabaseObjets.kt
+++ src/main/kotlin/com/skichrome/model/DatabaseObjets.kt
@@ -10,38 +10,38 @@ object Realty : Table()
     val id = long("id").primaryKey()
     val price = float("price")
     val surface = float("surface")
-    val roomNumber = integer("roomNumber")
-    val fullDescription = varchar("fullDescription", 256)
+    val roomNumber = integer("room_number")
+    val fullDescription = varchar("full_description", 256)
     val address = varchar("address", 128)
     val postCode = integer("postCode")
     val city = varchar("city", 64)
     val latitude = double("latitude").nullable()
     val longitude = double("longitude").nullable()
     val status = bool("status")
-    val dateAdded = date("dateAdded")
-    val dateSell = date("dateSell").nullable()
+    val dateAdded = date("date_added")
+    val dateSell = date("date_sell").nullable()
     val agentId = long("agent_id").references(Agent.agentId).index("agent_id_idx")
     val realtyTypeId = integer("realty_type_id").references(RealtyType.id).index("realty_type_idx")
 }
 
 object Poi : Table()
 {
-    val id = integer("poiId").primaryKey()
+    val id = integer("poi_id").primaryKey()
     val name = varchar("name", 64)
 }
 
 object RealtyType : Table()
 {
-    val id = integer("realtyTypeId").primaryKey()
+    val id = integer("realty_type_id").primaryKey()
     val name = varchar("name", 64)
 }
 
 object MediaReference : Table()
 {
-    val id = long("mediaReferenceId").primaryKey()
+    val id = long("media_reference_id").primaryKey()
     val reference = varchar("reference", 128)
-    val shortDesc = varchar("shortDesc", 256)
-    val realtyId = long("realtyId").references(
+    val shortDesc = varchar("short_description", 256)
+    val realtyId = long("realty_id").references(
             ref = Realty.id,
             onUpdate = ReferenceOption.CASCADE,
             onDelete = ReferenceOption.CASCADE
@@ -50,15 +50,15 @@ object MediaReference : Table()
 
 object PoiRealty : Table()
 {
-    val realtyId = long("realtyId").primaryKey(0).references(Realty.id)
-    val poiId = integer("poiId").primaryKey(1).references(Poi.id)
+    val realtyId = long("realty_id").primaryKey(0).references(Realty.id)
+    val poiId = integer("poi_id").primaryKey(1).references(Poi.id)
 }
 
 object Agent : Table()
 {
-    val agentId = long("agentId").primaryKey().autoIncrement()
+    val agentId = long("agent_id").primaryKey().autoIncrement()
     val name = varchar("name", 64)
-    val lastUpdate = datetime("lastUpdate").default(DateTime(System.currentTimeMillis()))
+    val lastUpdate = datetime("last_update").default(DateTime(System.currentTimeMillis()))
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
